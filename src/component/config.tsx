12b28933a1e5c477744e1528905958f66e553850
poorrigel <poorrigel@gmail.com>
2020-04-15 22:50:43 +0800

    feat:add config cmp

--- src/component/config.tsx
+++ src/component/config.tsx
@@ -1,7 +1,62 @@
 import * as React from 'react';
+import { Drawer, Form, Input, Button } from 'antd';
+import * as conf from '@/conf/elecodiConf';
 
-export default class Config extends React.Component {
-    render() {
-        return <div>Config</div>
-    }
+interface Props {
+    visible: boolean,
+    closeConfig: () => void
 }
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+};
+
+const Config = (props: Props) => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values: conf.Config) => {
+        console.log('Success:', values);
+        conf.setConfig(values);
+        props.closeConfig();
+    };
+    const onReset = () => {
+        form.resetFields();
+    }
+
+    const { visible } = props;
+    return (
+        <Drawer
+            title="配置"
+            width={500}
+            placement="right"
+            closable={false}
+            maskClosable={false}
+            visible={visible}
+        >
+            <Form
+                {...layout}
+                form={form}
+                onFinish={onFinish}>
+                <Form.Item name="playerCmd" label="playerCmd" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name="kodiHttpUrl" label="kodiHttpUrl" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name="kodiWsUrl" label="kodiWsUrl" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item {...tailLayout}>
+                    <Button htmlType="button" style={{marginRight: '10px'}} onClick={onReset}>重置</Button>
+                    <Button type="primary" htmlType="submit">提交</Button>
+                </Form.Item>
+            </Form>
+        </Drawer>
+    )
+}
+
+export default Config;
--- src/page/dashboard/index.tsx
+++ src/page/dashboard/index.tsx
@@ -1,18 +1,44 @@
 import * as React from 'react';
-import { Layout, Menu } from 'antd';
+import * as conf from '@/conf/elecodiConf';
+import Config from '@/component/config';
+import { Layout, Menu, Icon } from 'antd';
 
 const { Header, Footer, Sider, Content } = Layout;
 
+class DashboardState {
+    visible: boolean
+}
+
 export default class Dashboard extends React.Component {
+    state: DashboardState = {
+        visible: false
+    }
+    componentDidMount() {
+        let config = conf.getConfig();
+        if (Object.values(config).length === 0) {
+            this.setState({
+                visible: true
+            })
+        }
+    }
+    onCloseConfig = () => {
+        this.setState({
+            visible: false
+        })
+    }
     render() {
+        const { visible } = this.state;
         return (
             <Layout>
                 <Sider width={256} style={{ minHeight: '100vh', color: 'white' }}>
+                    <div style={{ height: '32px', background: 'rgba(255,255,255,.2)', margin: '16px'}}/>
                     <Menu theme="dark" mode="inline" defaultSelectedKeys={['movies']}>
                         <Menu.Item key="movies">
+                            <Icon type="pie-chart" />
                             <span>movies</span>
                         </Menu.Item>
                         <Menu.Item key="tv">
+                            <Icon type="pie-chart" />
                             <span>tv</span>
                         </Menu.Item>
                     </Menu>
@@ -26,6 +52,7 @@ export default class Dashboard extends React.Component {
                     </Content>
                     <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
                 </Layout>
+                <Config visible={visible} closeConfig={this.onCloseConfig}/>
             </Layout>
         )
     }
