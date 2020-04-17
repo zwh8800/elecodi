dcc49150dc9b903dfede106d63a0e2912751a5d9
wastecat <6185988+zwh8800@users.noreply.github.com>
2020-04-18 03:04:06 +0800

    grouping

--- src/page/dashboard/movies.tsx
+++ src/page/dashboard/movies.tsx
@@ -6,6 +6,8 @@ import { DownOutlined } from '@ant-design/icons';
 import Loader from 'react-loaders';
 import classnames from 'classnames';
 import InfiniteScroll from 'react-infinite-scroller';
+import pinyin from 'tiny-pinyin';
+import moment from 'moment';
 
 import Poster from '@/component/poster/poster';
 import * as conf from '@/conf/elecodiConf';
@@ -20,18 +22,31 @@ import '@/assets/style/media-list';
 
 const PAGE_SIZE = 20;
 
+class GroupInfo {
+    name: string
+}
+
+class GroupMedia<M> {
+    group: GroupInfo;
+    media: M[];
+}
+
+type GroupMovie = GroupMedia<Movie>
+
 interface Props { }
 
 class State {
     loading: boolean = false;
     isScollEnd: boolean = false;
-    movies: Movie[] = [];
-    sortMethod: SortMethod = SortMethod.dateadded;
-    sortOrder: SortOrder = SortOrder.Descending;
+    movies: GroupMovie[] = [];
+    sortMethod: SortMethod = SortMethod.sorttitle;
+    sortOrder: SortOrder = SortOrder.Ascending;
 }
 
 export default class Movies extends React.Component<Props, State> {
     curPage: number = 0;
+    movieBuffer: Movie[] = [];
+    lastBufferLength: number = 0;
 
     constructor(props: Props) {
         super(props);
@@ -43,26 +58,105 @@ export default class Movies extends React.Component<Props, State> {
         document.title = '电影 - ELECODI';
     }
 
+    getGroup(by: SortMethod, movie: Movie): GroupInfo {
+        switch (by) {
+            case SortMethod.dateadded: {
+                let date = new Date(movie.dateadded);
+                return {
+                    name: moment(date).format('YYYY-MM')
+                }
+            }
+
+            case SortMethod.sorttitle: {
+                if (!pinyin.isSupported()) {
+                    return null;
+                }
+                let tokens = pinyin.parse(movie.title.substr(0, 1));
+                if (tokens.length <= 0 || tokens[0].target.length <= 0) {
+                    return null;
+                }
+                if (tokens[0].type != 2) {
+                    return {
+                        name: '#'
+                    }
+                }
+                return {
+                    name: tokens[0].target.substr(0, 1).toUpperCase()
+                }
+            }
+
+            case SortMethod.lastplayed: {
+                let date = new Date(movie.lastplayed);
+                if (isNaN(date.getTime())) {
+                    return {
+                        name: '未播放'
+                    }
+                }
+                return {
+                    name: moment(date).format('YYYY-MM')
+                }
+            }
+
+            default:
+                return null;
+        }
+    }
+
+    grouping(by: SortMethod, groupMovie: GroupMovie[], movies: Movie[]): GroupMovie[] {
+        let lastGroup: GroupMovie;
+        if (groupMovie.length == 0) {
+            lastGroup = null;
+        } else {
+            lastGroup = groupMovie[groupMovie.length - 1]
+        }
+        for (let movie of movies) {
+            if (!movie) {
+                continue
+            }
+            let group = this.getGroup(by, movie);
+            if (group == null) {
+                lastGroup.media.push(movie);
+                continue;
+            }
+            if (lastGroup != null && group.name == lastGroup.group.name) {
+                lastGroup.media.push(movie);
+                continue;
+            }
+            lastGroup = {
+                group: group,
+                media: [movie]
+            }
+            groupMovie.push(lastGroup)
+        }
+
+        return groupMovie;
+    }
+
     async loadMovies() {
         this.setState({
             loading: true
         })
         try {
-            let movies = await getMovieLibrary({
+            let movieResp = await getMovieLibrary({
                 method: this.state.sortMethod,
                 order: this.state.sortOrder
             }, this.curPage, PAGE_SIZE);
-            if (movies.limits.end >= movies.limits.total) {
+            if (movieResp.limits.end >= movieResp.limits.total) {
                 this.setState({
                     isScollEnd: true
                 })
             }
-            for (let i = movies.limits.start; i < movies.limits.end; i++) {
-                this.state.movies[i] = movies.movies[i - movies.limits.start];
+            for (let i = movieResp.limits.start; i < movieResp.limits.end; i++) {
+                this.movieBuffer[i] = movieResp.movies[i - movieResp.limits.start];
             }
-            this.setState({
-                movies: this.state.movies
-            })
+            if ((this.movieBuffer.length - this.lastBufferLength) == Object.keys(this.movieBuffer).length) {
+                this.lastBufferLength = this.movieBuffer.length;
+                let newMovies = this.grouping(this.state.sortMethod, Array.of(...this.state.movies), this.movieBuffer);
+                this.movieBuffer = [];
+                this.setState({
+                    movies: newMovies,
+                })
+            }
 
         } catch (err) {
             console.error(err);
@@ -84,6 +178,7 @@ export default class Movies extends React.Component<Props, State> {
 
     reset() {
         this.curPage = 0;
+        this.lastBufferLength = 0;
 
         this.setState(new State());
         this.nextPage();
@@ -94,9 +189,9 @@ export default class Movies extends React.Component<Props, State> {
     }
 
     onSorterMenuSelected(param: ClickParam) {
-        console.log(param);
-
         this.curPage = 0;
+        this.lastBufferLength = 0;
+
         if (Object.values(SortMethod).includes(param.key as SortMethod)) {
             this.setState({
                 isScollEnd: false,
@@ -117,7 +212,7 @@ export default class Movies extends React.Component<Props, State> {
         const sorterMenu = (<Menu multiple={true} selectedKeys={[sortMethod, sortOrder]} onClick={this.onSorterMenuSelected.bind(this)}>
             <Menu.ItemGroup key="method" title="方式">
                 <Menu.Item key={SortMethod.dateadded}>
-                    按加入顺序
+                    按添加顺序
                 </Menu.Item>
                 <Menu.Item key={SortMethod.sorttitle}>
                     按名称
@@ -158,14 +253,24 @@ export default class Movies extends React.Component<Props, State> {
                     useWindow={false}
                 >
                     <div className="movie-list">
-                        {movies.map((movie) => (
-                            <Poster
-                                key={movie.movieid}
-                                identifier={movie.movieid}
-                                title={movie.title}
-                                plot={movie.plot}
-                                url={movie.art.poster}
-                            />
+                        {movies.map((group) => (
+                            <React.Fragment key={group.group.name}>
+                                {[...Array(20).keys()].map((i) => <i key={i}></i>)}
+                                {group.group ?
+                                    <div className="group-title">
+                                        {group.group.name}
+                                    </div> : null}
+
+                                {group.media.map((movie) => (
+                                    <Poster
+                                        key={movie.movieid}
+                                        identifier={movie.movieid}
+                                        title={movie.title}
+                                        plot={movie.plot}
+                                        url={movie.art.poster}
+                                    />
+                                ))}
+                            </React.Fragment>
                         ))}
                         {[...Array(20).keys()].map((i) => <i key={i}></i>)}
                     </div>
