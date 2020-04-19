d81048497ac69f1a1a0a8b9cb8c909d60a7dd2b9
poorrigel <poorrigel@gmail.com>
2020-04-19 22:46:42 +0800

    style filter blur

--- src/page/movie/detail.tsx
+++ src/page/movie/detail.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import * as conf from '@/conf/elecodiConf';
 import Poster from '@/component/poster/poster';
 import Cast from '@/component/cast/cast';
 import Summary from '@/component/summary/summary';
@@ -33,30 +34,34 @@ function Detail(props: Props) {
 
     }
 
-    return <div className="detail-con">
-        {
-            movie ? (
-                <React.Fragment>
-                    <div className="row-first">
-                        <Poster
-                            key={movie.movieid}
-                            identifier={movie.movieid}
-                            title={movie.title}
-                            plot={movie.plot}
-                            width={200}
-                            height={300}
-                            url={movie.art.poster}
-                            onClick={onMediaClick}
-                            onPlayClick={onPlayClick} />
-                        <Summary movie={movie} />
-                    </div>
-                    <div className="row-second">
-                        <Cast cast={movie.cast} />
-                    </div>
-                </React.Fragment>
-            ) : null
-        }
+    function transKodiImage(url: string) {
+        url = encodeURIComponent(url);
+        return conf.getConfig().kodiHttpUrl + '/image/' + url;
+    }
+
+
+    return movie ? (<div className="detail-con" style={{ backgroundImage: `url('${transKodiImage(movie.art.fanart)}')` }}>
+        <div className="detail-scroll-con">
+            <div className="row-first">
+                <Poster
+                    key={movie.movieid}
+                    identifier={movie.movieid}
+                    title={movie.title}
+                    plot={movie.plot}
+                    width={200}
+                    height={300}
+                    url={movie.art.poster}
+                    onClick={onMediaClick}
+                    onPlayClick={onPlayClick} />
+                <Summary movie={movie} />
+            </div>
+            <div className="row-second">
+                <Cast cast={movie.cast} />
+            </div>
+        </div>
     </div>
+    ) : null
+
 }
 
 export default Detail;
