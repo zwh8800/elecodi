efb4759d36e26e0c95cb3482e505126d07896c38
wastecat <6185988+zwh8800@users.noreply.github.com>
2020-04-20 18:04:29 +0800

    audio video

--- src/component/summary/summary.tsx
+++ src/component/summary/summary.tsx
@@ -18,8 +18,55 @@ function Summary(props: Props) {
         return duration.minutes() + '分钟';
     }
 
-    const { title, originaltitle, year, runtime, lastplayed, rating, genre, director, country, imdbnumber, plot } = props.movie;
+    const aspectList = [4 / 3, 16 / 9, 20 / 9, 21.5 / 9];
+    const aspectStrList = ["4:3", "16:9", "20:9", "21.5:9"];
 
+    function aspectFormat(aspect: number) {
+        let minIndex = 0;
+        let minDiff = 999999;
+        for (let [i, a] of aspectList.entries()) {
+            let diff = Math.abs(a - aspect);
+            if (diff < minDiff) {
+                minIndex = i;
+                minDiff = diff;
+            }
+        }
+        return aspectStrList[minIndex];
+    }
+
+    const dimensionList = [640, 1280, 1920, 3840];
+    const dimensionStrList = ["SD", "720p", "1080p", "4k"];
+
+    function dimensionFormat(width: number, height: number) {
+        let minIndex = 0;
+        let minDiff = 999999;
+        for (let [i, d] of dimensionList.entries()) {
+            let diff = Math.abs(d - width);
+            if (diff < minDiff) {
+                minIndex = i;
+                minDiff = diff;
+            }
+        }
+        return dimensionStrList[minIndex];
+    }
+
+    function audioFormat(channel: number) {
+        switch (channel) {
+            case 8:
+                return "7.1";
+            case 6:
+                return "5.1";
+            case 3:
+                return "2.1";
+            case 2:
+            default:
+                return "2.0";
+            case 1:
+                return "mono";
+        }
+    }
+
+    const { title, originaltitle, year, runtime, lastplayed, rating, genre, director, country, imdbnumber, plot, streamdetails } = props.movie;
 
     return (
         <div className="summary">
@@ -36,7 +83,7 @@ function Summary(props: Props) {
                         <span className="duration">{durationForamt(runtime)}</span>
                         <span className="dot">·</span>
                         <span>{lastplayed ? '已播放' : '未播放'}</span>
-                        <span className="rate">{rating ? `${rating} / 10` : '未评分'}</span>
+                        <span className="tag-label">{rating ? `${rating} / 10` : '未评分'}</span>
                     </div>
                     <p className="right">{(genre || []).join(',')}</p>
 
@@ -58,20 +105,69 @@ function Summary(props: Props) {
                         <span className="value">{imdbnumber}</span>
                     </div>
                 </section>
-                {/* <section className="section">
-                    <div className="item">
-                        <span className="label">视频</span>
-                        <span className="value">222</span>
-                    </div>
-                    <div className="item">
-                        <span className="label">音频</span>
-                        <span className="value">22</span>
-                    </div>
-                    <div className="item">
-                        <span className="label">字幕</span>
-                        <span className="value">22</span>
-                    </div>
-                </section> */}
+                {streamdetails ? (
+                    <section className="section">
+                        <div className="item">
+                            <span className="label">视频</span>
+                            <span className="value">
+                                {
+                                    streamdetails.video.length > 0 ?
+                                        <React.Fragment>
+                                            <span className="tag-label">
+                                                {streamdetails.video[0].codec}
+                                            </span>
+                                            <span className="tag-label">
+                                                {dimensionFormat(streamdetails.video[0].width, streamdetails.video[0].height)}
+                                            </span>
+                                            <span className="tag-label">
+                                                {aspectFormat(streamdetails.video[0].aspect)}
+                                            </span>
+                                        </React.Fragment>
+                                        : ""
+                                }
+                            </span>
+                        </div>
+                        <div className="item">
+                            <span className="label">音频</span>
+                            <span className="value">
+                                {
+                                    streamdetails.audio.map((audio, i) => <React.Fragment>
+                                        <span className="tag-label">
+                                            {audio.codec}
+                                        </span>
+                                        <span className="tag-label">
+                                            {audioFormat(audio.channels)}
+                                        </span>
+                                        {
+                                            audio.language ?
+                                                <span className="tag-label">
+                                                    {audio.language}
+                                                </span>
+                                                : ""
+                                        }
+                                        {
+                                            i != streamdetails.audio.length - 1 ?
+                                                <span> | </span>
+                                                : ""
+                                        }
+                                    </React.Fragment>
+                                    )
+                                }
+                            </span>
+                        </div>
+                        <div className="item">
+                            <span className="label">字幕</span>
+                            <span className="value">
+                                {
+                                    streamdetails.subtitle.map(sub => <span className="tag-label">
+                                        {sub.language}
+                                    </span>
+                                    )
+                                }
+                            </span>
+                        </div>
+                    </section>
+                ) : ""}
                 <section className="section introduction">
                     <p>{plot}</p>
                 </section>
